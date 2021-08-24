c11d60c99779742d946b051cc991a8132968197f
Aku1795 <mtvc95@gmail.com>
2021-08-24 08:13:03 +0200

    reformating

--- Makefile
+++ Makefile
@@ -1,4 +1,18 @@
 connect-to-db:
 	docker exec -it postgres psql -U postgres -d postgres
 
+setup-environment:
+	pip3 install virtualenv==20.0.15
+	virtualenv env
+	source env/bin/activate; \
+	pip3 install -r requirements.txt
 
+format:
+	@echo Formatting...
+	source env/bin/activate; \
+	python -m black application/
+
+lint: format
+	@echo Linting...
+	source env/bin/activate; \
+	python -m flake8 application/
--- application/app.py
+++ application/app.py
@@ -7,34 +7,37 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import desc
 
-#app
+# app
 app = Flask(__name__)
 
-#DB connection
-user = os.environ['POSTGRES_USER']
-password = os.environ['POSTGRES_PASSWORD']
-host = os.environ['POSTGRES_HOST'] 
-database = os.environ['POSTGRES_DB']
-port = os.environ['POSTGRES_PORT']
+# DB connection
+user = os.environ["POSTGRES_USER"]
+password = os.environ["POSTGRES_PASSWORD"]
+host = os.environ["POSTGRES_HOST"]
+database = os.environ["POSTGRES_DB"]
+port = os.environ["POSTGRES_PORT"]
 
-DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION_URI
+DATABASE_CONNECTION_URI = (
+    f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_CONNECTION_URI
 
 
 db = SQLAlchemy(app)
 
-#DB models
+# DB models
 Base = automap_base()
-Base.prepare(db.engine, reflect =True)
+Base.prepare(db.engine, reflect=True)
 TemperaturesByCity = Base.classes.temperatures_by_time_and_city
 
 
-#Routes
-@app.route('/')
+# Routes
+@app.route("/")
 def index():
-    return 'Hello world'
+    return "Hello world"
 
-@app.route('/add_city', methods=['POST'])
+
+@app.route("/add_city", methods=["POST"])
 def add_city():
 
     data = request.get_json()
@@ -47,20 +50,21 @@ def add_city():
     longitude = data["longitude"]
 
     new_city = TemperaturesByCity(
-        date= date
-        ,avg_temperature= avg_temperature
-        ,avg_temperature_uncertainty= avg_temperature_uncertainty
-        ,city= city
-        ,country= country
-        ,latitude= latitude
-        ,longitude= longitude
+        date=date,
+        avg_temperature=avg_temperature,
+        avg_temperature_uncertainty=avg_temperature_uncertainty,
+        city=city,
+        country=country,
+        latitude=latitude,
+        longitude=longitude,
     )
     db.session.add(new_city)
     db.session.commit()
 
     return f"{city} added"
 
-@app.route('/modify_city', methods=['POST'])
+
+@app.route("/modify_city", methods=["POST"])
 def modify_city():
 
     data = request.get_json()
@@ -70,57 +74,62 @@ def modify_city():
     city = data["city"]
     country = data["country"]
 
-
-    query = db.session\
-        .query(TemperaturesByCity)\
-        .filter(TemperaturesByCity.date==date)\
-        .filter(TemperaturesByCity.city==city)\
-        .filter(TemperaturesByCity.country==country)
+    query = (
+        db.session.query(TemperaturesByCity)
+        .filter(TemperaturesByCity.date == date)
+        .filter(TemperaturesByCity.city == city)
+        .filter(TemperaturesByCity.country == country)
+    )
 
     if avg_temperature is not None:
         query.update({TemperaturesByCity.avg_temperature: avg_temperature})
-    
+
     if avg_temperature_uncertainty is not None:
-        query.update({TemperaturesByCity.avg_temperature_uncertainty: avg_temperature_uncertainty})
-        
+        query.update(
+            {
+                TemperaturesByCity.avg_temperature_uncertainty: avg_temperature_uncertainty
+            }
+        )
+
     db.session.commit()
 
     return f"{city} modified"
 
-@app.route('/fetch_top_cities', methods=['POST'])    
+
+@app.route("/fetch_top_cities", methods=["POST"])
 def fetch_top_cities():
-    
-    
+
     data = request.get_json()
     print(data)
     n_rows = data["n_rows"]
     start_date = data["start_date"]
     end_date = data["end_date"]
 
-    query = db.session\
-        .query(TemperaturesByCity)\
-        .filter(TemperaturesByCity.avg_temperature != None)\
-        .filter(TemperaturesByCity.date>=start_date)\
-        .filter(TemperaturesByCity.date<=end_date)\
-        .order_by(desc(TemperaturesByCity.avg_temperature))\
-        .limit(int(n_rows))\
+    query = (
+        db.session.query(TemperaturesByCity)
+        .filter(TemperaturesByCity.avg_temperature != None)
+        .filter(TemperaturesByCity.date >= start_date)
+        .filter(TemperaturesByCity.date <= end_date)
+        .order_by(desc(TemperaturesByCity.avg_temperature))
+        .limit(int(n_rows))
         .all()
-    
+    )
+
     results = []
     for result in query:
-        new_row= {
-        "date": result.date.strftime("%Y-%m-%d")
-        ,"avg_temperature": str(result.avg_temperature)
-        ,"avg_temperature_uncertainty": str(result.avg_temperature_uncertainty)
-        ,"city": result.city
-        ,"country": result.country
-        ,"latitude": result.latitude
-        ,"longitude": result.longitude
+        new_row = {
+            "date": result.date.strftime("%Y-%m-%d"),
+            "avg_temperature": str(result.avg_temperature),
+            "avg_temperature_uncertainty": str(result.avg_temperature_uncertainty),
+            "city": result.city,
+            "country": result.country,
+            "latitude": result.latitude,
+            "longitude": result.longitude,
         }
         results.append(new_row)
 
     return json.dumps(results)
 
 
-if __name__ == '__main__':
-    app.run(debug= True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
